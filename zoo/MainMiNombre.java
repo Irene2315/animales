4b938d10350c21ef04d164e6acacd6a75519692c
Irene Herrero <Irene Herrero@HZ114831.H012108.NET>
2022-12-13 14:28:00 +0100

    mi nombre terminado

---
+++ zoo/MainMiNombre.java
@@ -0,0 +1,18 @@
+
+public class mainMiNombre {
+
+	public static void main(String[] args) {
+		AnguilaElectrica anguilas = new AnguilaElectrica();
+		anguilas.presentarme();
+		System.out.println();
+		
+		Atun atunes = new Atun();
+		atunes.presentarme();
+		System.out.println();
+		
+		caballitoDeMar caballito = new caballitoDeMar();
+		caballito.presentarme();
+
+	}
+
+}
